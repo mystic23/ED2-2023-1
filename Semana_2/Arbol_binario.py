ceca49fed74d68aaa2454f7666920ef566fd4bec
María Isabel Solá Valle <msola@uninorte.edu.co>
2023-02-01 17:08:53 -0500

    Corrección sin el current

--- Semana_2/Arbol_binario.py
+++ Semana_2/Arbol_binario.py
@@ -3,21 +3,20 @@ class TreeNode:
         self.left = None
         self.right = None
         self.value = value
-        self.content = None
+   
         
-    def insert(self, value, content = None):
+    def insert(self, value):
         if value < self.value:
             if self.left is None:
                 self.left = TreeNode(value)
-                self.left.content = content
+                
             else:
-                self.left.insert(value, content)
+                self.left.insert(value)
         else: 
             if self.right is None:
                 self.right = TreeNode(value)
-                self.right.content = content
             else:
-                self.right.insert(value, content )
+                self.right.insert(value)
     
     def inorder_traversal(self): 
         """
@@ -58,17 +57,17 @@ class TreeNode:
     def find(self, value):
             if value < self.value:
                 if self.left is None:
-                    return None
+                    return True
                 else:
                     return self.left.find(value)
             elif value > self.value:
                 if self.right is None:
-                    return None
+                    return True
                 else:
                     return self.right.find(value)
             else:
                 ##Aquí es donde lo encuentra
-                return self.value      
+                return True
                 
 
 #probando el arbolito, forma mundana y brusca 
@@ -78,14 +77,13 @@ tree.insert(4)
 tree.insert(3)
 tree.insert(1)
 tree.insert(6)
-tree.insert(9 , { "Jesus", " Te amo mucho cito <3 "})
+tree.insert(9)
 tree.insert(12)
 tree.insert(13)
 tree.insert(18)
 tree.insert(25)
 tree.insert(15)
 
-#tree.postorder_traversal()
-print(tree.find(9).content['Jesus'])
+#tree.postorder_traversal()                                                                                                                                                                             
+print(tree.find(9))
 
-myList = [5,1,34,44,2,3,5,9,2,11,12,8]
